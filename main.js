ae119cc515fc7f32465b85c3fc7f9aabaf2c407f
Chadi <chadihourani11@gmail.com>
2020-08-06 20:43:41 +0300

    Removed template literals and added Delete button

--- main.js
+++ main.js
@@ -34,23 +34,67 @@ function saveIssue(e) {
 function setStatusClosed(id) {
     var issues = JSON.parse(localStorage.getItem('issues'));
     
-    for (var i = 0; i<= issues.length; i++){
+    for (var i = 0; i< issues.length; i++){
         if (issues[i].id == id){
             issues[i].status = 'Closed';
         }
     }
+
     localStorage.setItem('issues', JSON.stringify(issues));
     fetchIssues();
 }
 
-function fetchIssues(){
+function deleteIssue(id) {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+  
+    for (var i = 0; i < issues.length; i++) {
+      if (issues[i].id == id) {
+        issues.splice(i, 1);
+      }
+    }
+  
+    localStorage.setItem('issues', JSON.stringify(issues));
+  
+    fetchIssues();
+  }
+
+function fetchIssues() {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    var issuesList = document.getElementById('issuesList');
+  
+    issuesList.innerHTML = '';
+  
+    issues.forEach(issue => {
+        let id = issue.id;
+        let desc = issue.description;
+        let severity = issue.severity;
+        let assignedTo = issue.assignedTo;
+        let status = issue.status;
+  
+      issuesList.innerHTML += '<div class="well">'+
+                              '<h6>Issue ID: ' + id + '</h6>'+
+                              '<p><span class="label label-info">' + status + '</span></p>'+
+                              '<h3>' + desc + '</h3>'+
+                              '<p><span class="glyphicon glyphicon-time"></span> ' + severity + '</p>'+
+                              '<p><span class="glyphicon glyphicon-user"></span> ' + assignedTo + '</p>'+
+                              '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a> '+
+                              '<a href="#" onclick="deleteIssue(\''+id+'\')" class="btn btn-danger">Delete</a>'+
+                              '</div>';
+    })
+}
+
+  
+
+
+/*function fetchIssues(){
     let issues = JSON.parse(localStorage.getItem('issues'));
     let issuesList = document.getElementById('issuesList');
 
     issuesList.innerHTML = '';
 
+    if(issues){
     issues.forEach(issue => {
-        let id = issue.id;
+        var id = issue.id;
         let desc = issue.description;
         let severity = issue.severity;
         let assignedTo = issue.assignedTo;
@@ -60,11 +104,12 @@ function fetchIssues(){
                                  <h6>Issue ID: ${id} </h6>
                                  <p><span class="label label-info"> ${status} </span></p>
                                  <h3>${desc}</h3>
-                                 <p><span class="glyphicon glyphicon-time"</span> ${severity}
-                                 <span class="glyphicon glyphicon-user"</span> ${assignedTo} </p>
+                                 <p><span class="glyphicon glyphicon-time"</span> ${severity}</p>
+                                 <p><span class="glyphicon glyphicon-user"</span> ${assignedTo} </p>
                                  <a href='#' onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
                                  <a href="#"  onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                                  </div>
                                  `;
     });
 }
+}*/
