dccfdb4d780436ea5658771620767f3fb633a105
Chadi <chadihourani11@gmail.com>
2020-08-06 18:09:19 +0300

    Changing variables declarations

--- main.js
+++ main.js
@@ -32,11 +32,14 @@ function saveIssue(e) {
 }
 
 function setStatusClosed(id) {
-    issues.forEach(issue =>{
-        if (issue.id == id){
-            issue.status = 'Closed';
+    var issues = JSON.parse(localStorage.getItem('issues'));
+    
+    for (var i = 0; i<= issues.length; i++){
+        if (issues[i].id == id){
+            issues[i].status = 'Closed';
         }
-    });
+    }
+    localStorage.setItem('issues', JSON.stringify(issues));
     fetchIssues();
 }
 
