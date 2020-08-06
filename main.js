cd9e674e8e7bf99861fe79b351369c1bf77191ec
Chadi <chadihourani11@gmail.com>
2020-08-06 16:34:14 +0300

    Adding button eventlisterners

--- main.js
+++ main.js
@@ -22,8 +22,22 @@ function saveIssue(e) {
     } else {
         var issues = JSON.parse(localStorage.getItem('issues'));
         issues.push(issue);
+        localStorage.setItem('issues', JSON.stringify(issues));
     }
     
+    document.getElementById('issueInputForm').reset();
+
+    fetchIssues();
+    e.preventDefault();
+}
+
+function setStatusClosed(id) {
+    issues.forEach(issue =>{
+        if (issue.id == id){
+            issue.status = 'Closed';
+        }
+    });
+    fetchIssues();
 }
 
 function fetchIssues(){
@@ -43,8 +57,8 @@ function fetchIssues(){
                                  <h6>Issue ID: ${id} </h6>
                                  <p><span class="label label-info"> ${status} </span></p>
                                  <h3>${desc}</h3>
-                                 <p><span class="glyphicon glyphicon-time"</span> ${severity} </p>
-                                 <p><span class="glyphicon glyphicon-user"</span> ${assignedTo} </p>
+                                 <p><span class="glyphicon glyphicon-time"</span> ${severity}
+                                 <span class="glyphicon glyphicon-user"</span> ${assignedTo} </p>
                                  <a href='#' onclick="setStatusClosed(${id})" class="btn btn-warning">Close</a>
                                  <a href="#"  onclick="deleteIssue(${id})" class="btn btn-danger">Delete</a>
                                  </div>
