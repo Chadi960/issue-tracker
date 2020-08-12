44936c4b2efbe36e6353349176aaefedda12f362
Chadi <chadihourani11@gmail.com>
2020-08-12 19:30:30 +0300

    last state

--- main.js
+++ main.js
@@ -74,8 +74,8 @@ function fetchIssues() {
                                  <h6>Issue ID: ${id} </h6>
                                  <p><span class="label label-info"> ${status} </span></p>
                                  <h3>${desc}</h3>
-                                 <p><span class="glyphicon glyphicon-time"</span> ${severity}</p>
-                                 <p><span class="glyphicon glyphicon-user"</span> ${assignedTo} </p>
+                                 <p><span class="glyphicon glyphicon-time"</span> ${severity}
+                                 <span class="glyphicon glyphicon-user"</span> ${assignedTo} </p>
                                  <a href='#' onclick="setStatusClosed('${id}')" class="btn btn-warning">Close</a>
                                  <a href="#"  onclick="deleteIssue('${id}')" class="btn btn-danger">Delete</a>
                                  </div>
